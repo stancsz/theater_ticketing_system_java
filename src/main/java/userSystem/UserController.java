9e056107d811fb697ae1fa94785d5bec4d2f9232
schaykovskyy <4.chaykovskyy@gmail.com>
2021-12-06 03:03:51 -0700

    Changed bookings table in DB
    Modified bookings table to contiant bookingsId, userId and ticketId.
    Added Comments to UserController.java

--- src/main/java/userSystem/UserController.java
+++ src/main/java/userSystem/UserController.java
@@ -4,47 +4,63 @@ import java.util.ArrayList;
 import java.sql.*;
 
 public class UserController{
+    public static void main(String[] args) {
+        UserController uc = new UserController();
+    }
+
     private ArrayList<RegisteredUser> listOfRegisteredUsers;
     private ArrayList<User> listOfUsers;
-     
+    /**
+     * Constructor. creates listOfRegisteredUsers and listOfUsers.
+     *  loads all users from db
+     */ 
     public UserController(){
         listOfRegisteredUsers = new ArrayList<RegisteredUser>();
         listOfUsers = new ArrayList<User>();
         try {
             loadAllUsers();
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }    
 
-    public static void main(String[] args) {
-        System.out.println("It works");
-        UserController uc = new UserController();
-
-        uc.deleteUser(3);
-    }
-
+    /**
+     *  Creates registeredUser and adds it to listOfRegisteredUsers 
+     *  Adds it to DB
+     * @param userId 
+     * @param eMail
+     * @param name
+     * @param address
+     */
     public void addRegisteredUser(int userId,String eMail, String name, String address){
         RegisteredUser ru = new RegisteredUser(name, address, userId, eMail);
         listOfRegisteredUsers.add(ru);
         try {
             addRegisteredUserToDB(userId, eMail, name, address);
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
+    /**
+     * Creates regular user and adds it to listOfUsers
+     * Adds it to DB
+     * @param userId
+     * @param eMail
+     */
     public void addUser(int userId, String eMail){
         User u = new User(userId, eMail);
         listOfUsers.add(u);
         try {
             addUserToDB(userId, eMail);
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
+    /**
+     * Finds and delets user object.
+     * Deletes it from DB
+     * @param userId
+     */
     public void deleteUser(int userId){
         //find and remove user object
         for (RegisteredUser ru: listOfRegisteredUsers){
@@ -63,40 +79,73 @@ public class UserController{
         try{
             deleteUserFromDB(userId);
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Adds ticket to users listOfTickets
+     * Adds ticket booking to DB
+     * @param user
+     * @param ticket
+     */
+    public void addTicketBooking(User user, Ticket ticket){
+        user.getListOfTickets().add(ticket);
+        try {
+            addTicketBookingToDB(user.getUserId(), ticket.getTicketId());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Removes ticket from users listOfTickets
+     * Removes ticket booking from DB
+     * @param user
+     * @param ticket
+     */
+    public void removeTicketBooking(User user, Ticket ticket){
+        user.getListOfTickets().remove(ticket);
+        try {
+            removeTicketBookingFromDB(user.getUserId(), ticket.getTicketId());
+        } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-    public void addTicket(User user, Ticket ticket){
-        user.getListOfTickets().add(ticket);
-    }
-
-    public void removeTicket(User user, Ticket ticket){
-        user.getListOfTickets().remove(ticket);
-    }
-    // public void addRefundCredit(User user, Ticket ticket){
-    //     // we dont have price of a ticket 
-    //     Credit credit = new Credit(ticket.getPrice(), ticket.getShowtime().getStartTime());
-    //     user.getListOfCredits().add(credit);
-    // }
-
+    /**
+     * Getter for listOfRegisteredUsers
+     * @return listOfRegisteredUsers
+     */
     public ArrayList<RegisteredUser> getListOfRegisteredUsers() {
         return this.listOfRegisteredUsers;
     }
-
+    /**
+     * Setter for list of registeredUsers
+     * @param listOfRegisteredUsers
+     */
     public void setListOfRegisteredUsers(ArrayList<RegisteredUser> listOfRegisteredUsers) {
         this.listOfRegisteredUsers = listOfRegisteredUsers;
     }
 
+    /**
+     * Getter for listOfUsers
+     * @return listOfUsers
+     */
     public ArrayList<User> getListOfUsers() {
         return this.listOfUsers;
     }
-
+    /**
+     * Setter for listOfUsers
+     * @param listOfUsers
+     */ 
     public void setListOfUsers(ArrayList<User> listOfUsers) {
         this.listOfUsers = listOfUsers;
     }
-    //-----------------------------Methods to work with DB----------------------
-    //  Connect to a sample database
+    //--------------------------------------Methods to work with DB----------------------------------------------
+    /**
+     * Connects to sqlite.db 
+     * @return Connection
+     */
     public Connection connect() {
         Connection conn = null;
         try {
@@ -110,7 +159,10 @@ public class UserController{
         }
         return conn;
     }
-
+    /**
+     * Load all users from DB to listOfUsers and listOfRegiesteredUsers
+     * @throws SQLException
+     */
     private void loadAllUsers() throws SQLException{
         Connection conn = connect();
         String sql = String.format("SELECT * FROM Users"); 
@@ -133,6 +185,12 @@ public class UserController{
         }
         conn.close();
     }
+    /**
+     * Adds user to DB
+     * @param userId
+     * @param eMail
+     * @throws SQLException
+     */
     public void addUserToDB(int userId, String eMail) throws SQLException{
         Connection conn = connect();
 
@@ -141,6 +199,9 @@ public class UserController{
         int rs = stmt.executeUpdate(sql);
         conn.close();
     }
+    /**
+     * Adds registeredUser to DB
+     */
     public void addRegisteredUserToDB(int userId, String eMail, String name, String address) throws SQLException{
         Connection conn = connect();
 
@@ -149,7 +210,11 @@ public class UserController{
         int rs = stmt.executeUpdate(sql);
         conn.close();
     }
-    //deletes both Registered and regular user from db
+    /**
+     * Deletes User or RegisteredUser from DB
+     * @param userId
+     * @throws SQLException
+     */
     public void deleteUserFromDB(int userId) throws SQLException{
         Connection conn = connect();
 
@@ -158,4 +223,34 @@ public class UserController{
         int rs = stmt.executeUpdate(sql);
         conn.close();
     }
+    /**
+     * Adds ticket booking to DB
+     * @param userId
+     * @param ticketId
+     * @throws SQLException
+     */
+    public void addTicketBookingToDB(int userId, int ticketId)throws SQLException{
+        Connection conn = connect();
+        long bookingId= System.currentTimeMillis();
+
+        String sql = String.format("INSERT INTO bookings (BookingId, userId, ticketId) VALUES (%d ,%d,%d)",bookingId, userId, ticketId); 
+        Statement stmt = conn.createStatement();
+        int rs = stmt.executeUpdate(sql);
+        conn.close();
+    }
+    /**
+     * Remove ticket booking from DFB
+     * @param userId
+     * @param ticketId
+     * @throws SQLException
+     */
+    public void removeTicketBookingFromDB(int userId, int ticketId) throws SQLException{
+        Connection conn = connect();
+
+        String sql = String.format("DELETE FROM bookings WHERE userId= '%s' AND ticketId = '%s'", userId, ticketId); 
+        Statement stmt = conn.createStatement();
+        int rs = stmt.executeUpdate(sql);
+        conn.close();
+    }
 }
+
